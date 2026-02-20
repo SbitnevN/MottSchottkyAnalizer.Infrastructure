166aaf95b368b0713d7856c3da75aca3e1c75961
nikita <sbitnevn@yandex.ru>
2026-02-20 22:00:46 +0300

    Рефакторинг + новая библиотека

--- Dialogs/DialogService.cs
+++ Dialogs/DialogService.cs
@@ -1,24 +1,23 @@
-﻿using MottSchottkyAnalizer.Core.Dialogs;
-using MottSchottkyAnalizer.DI.Injection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MottSchottkyAnalizer.Core.Dialogs;
 using MottSchottkyAnalizer.DI.Registration;
 using System.Windows;
 
 namespace MottSchottkyAnalizer.Infrastructure.Dialogs;
 
 [Service<IDialogService>]
-internal class DialogService(IAppProvider serviceProvider) : IDialogService
+internal class DialogService(IServiceProvider serviceProvider) : IDialogService
 {
-    private readonly IAppProvider _serviceProvider = serviceProvider;
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
 
     public bool Show<T>(DialogParameters? parameters = null) where T : Window
     {
         T dialog = _serviceProvider.GetRequiredService<T>();
         DialogViewModel viewModel = (DialogViewModel)dialog.DataContext;
         viewModel.Parameters = parameters ?? new DialogParameters();
-        viewModel.OnChoose += () => dialog.Close();
+        viewModel.OnChoose += dialog.Close;
 
         dialog.ShowDialog();
-
         return viewModel.DialogResult;
     }
 }
--- Files/IParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using ElinsDataParser.Data;
-
-namespace MottSchottkyAnalizer.Infrastructure.Files;
-
-public interface IParser
-{
-    ElinsData Parse(string path);
-    Task<ElinsData> ParseAsync(string path);
-}
